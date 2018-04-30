8ce20017f68bb2d4b3b54b1927e1d4facc5aac1c
Ball-Man <francesco.mistri98@gmail.com>
2018-04-30 20:46:09 +0200

    List: better methods
    Fixed some bad design It now has more methods, and is no longer generic since its used as a non-homogeneus list.

--- src/list/Listable.java
+++ src/list/Listable.java
@@ -1,6 +1,13 @@
 package list;
 
-public interface Listable<T> {
-  public Listable<T> getNext();
+public interface Listable {
+  // Get / Set next
+  public Listable getNext();
+  public void setNext(Listable next);
+
+  // Get if finished
   public boolean getFinished();
+
+  // Insert / Remove
+  public Listable remove();
 }
