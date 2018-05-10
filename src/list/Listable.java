8e4503728f305c99f47992aa8c7115149ef54e1a
Ball-Man <francesco.mistri98@gmail.com>
2018-05-10 18:04:01 +0200

    getList for Listables
    Listables now have the default method 'getList' which returns an array containing all the elements in the list from the caller element on.

--- src/list/Listable.java
+++ src/list/Listable.java
@@ -10,4 +10,23 @@ public interface Listable {
 
   // Insert / Remove
   public Listable removeNext();
+
+  // Get an array containing the list of next items
+  public default Listable[] getList() {
+    // Count items
+    int count = 0;
+    Listable head = this;
+    for (; !head.getFinished(); count++)
+      head = head.getNext();
+
+    // Save actual items
+    head = this;
+    Listable[] items = new Listable[count];
+    for (int i = 0; i < count; i++) {
+      head = head.getNext();
+      items[i] = head;
+    }
+
+    return items;
+  }
 }
